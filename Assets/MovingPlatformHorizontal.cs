10bf90ce7ec54e24cbbeedf57dc2317a5421b2d0
kevinAK99 <yl6260@nyu.edu>
2021-04-17 00:40:56 +0800

    Moving platform scripts added
    Kevin's commit: Added the scripts for the horizontal and vertical moving platforms. Created a test moving platform in the sample scene.

---
+++ Assets/MovingPlatformHorizontal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformHorizontal : MonoBehaviour
+{
+    private Rigidbody2D m_RigidBody;
+    [SerializeField] private float Speed = 1.0f;
+    [SerializeField] private Vector3 M_Dir = Vector3.zero;
+
+    [SerializeField] float LeftX;
+    [SerializeField] float RightX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Vector3 velocity;
+        velocity = Speed * M_Dir;
+
+        m_RigidBody.velocity = velocity;
+
+        if (transform.position.x <= LeftX)
+        {
+            M_Dir = Vector3.right;
+
+        }
+
+        if (transform.position.x >= RightX)
+        {
+            M_Dir = Vector3.left;
+
+        }
+    }
+
+
+}
---
+++ Assets/MovingPlatformVertical.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformVertical : MonoBehaviour
+{
+    private Rigidbody2D m_RigidBody;
+    [SerializeField] private float Speed = 1.0f;
+    [SerializeField] private Vector3 M_Dir = Vector3.zero;
+
+    [SerializeField] float topY;
+    [SerializeField] float bottomY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 velocity;
+        velocity = Speed * M_Dir;
+
+        m_RigidBody.velocity = velocity;
+
+        if (transform.position.y >= topY)
+        {
+            M_Dir = Vector3.down;
+
+        }
+
+        if (transform.position.y <= bottomY)
+        {
+            M_Dir = Vector3.up;
+
+        }
+    }
+
+}
