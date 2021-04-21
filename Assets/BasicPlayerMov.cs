4a07db9ee7c48a002d97a7a7de840d506e1ab7ad
Jeremy Huml <jh5293@nyu.edu>
2021-04-21 10:24:52 -0400

    Simple Player Movement
    -Created a player with a simplified movement script
    -Added in complex movement script, but it's not yet adjusted to 2D physics

---
+++ Assets/BasicPlayerMov.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerMov : MonoBehaviour
+{
+    public float JumpForce = 1f;
+    public float GroundSpeed = 1f;
+    public bool CanJump = true;
+    public float RaycastDistance = 1f;
+
+    public Rigidbody2D rb;
+
+    bool leftInput = false;
+    bool rightInput = false;
+    bool jumpInput = false;
+    bool fallInput = false;
+
+    bool movingLeft = false;
+    bool movingRight = false;
+
+    
+
+    void Start()
+    {
+        {
+            rb = gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    void Update()
+    {
+        RegisterInputs();
+        DetermineMovement();
+        HorizontalMovement();
+        if (jumpInput) Jumping();
+    }
+
+    void RegisterInputs()
+    {
+        leftInput = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        rightInput = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        jumpInput = Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
+        fallInput = Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S);
+    }
+
+    void DetermineMovement()
+    {
+        movingLeft = leftInput && !rightInput;
+        movingRight = rightInput && !leftInput;
+    }
+
+    void HorizontalMovement()
+    {
+        if (movingLeft && !WallDetection(Vector3.left))
+        {
+            if (CanJump) transform.position += new Vector3(-GroundSpeed * Time.deltaTime, 0, 0);
+            else transform.position += new Vector3((-GroundSpeed / 2) * Time.deltaTime, 0, 0);
+        }
+        if (movingRight && !WallDetection(Vector3.right))
+        {
+            if (CanJump) transform.position += new Vector3(GroundSpeed * Time.deltaTime, 0, 0);
+            else transform.position += new Vector3((GroundSpeed / 2) * Time.deltaTime, 0, 0);
+        }
+    }
+
+    bool WallDetection(Vector3 direction)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, RaycastDistance, 255)) return true;
+        else return false;
+    }
+
+    void Jumping()
+    {
+        if (CanJump)
+        {
+            rb.AddForce(new Vector2(0, JumpForce));
+            CanJump = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!CanJump && collision.gameObject.CompareTag("Wall"))
+        {
+            CanJump = true;
+        }
+    }
+}
---
+++ Assets/Player_Control.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float GroundSpeed = 1f;
+    public bool CanJump = true;
+    public float RaycastDistance = 1f;
+
+    public bool WallCling = false;
+
+    bool leftInput = false;
+    bool rightInput = false;
+    bool jumpInput = false;
+    bool fallInput = false;
+
+    bool movingLeft = false;
+    bool movingRight = false;
+
+    public float JumpForce = 1f;
+    public float ArtificialGravity = 1f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        RegisterInputs();
+        DetermineMovement();
+        HorizontalMovement();
+        if (jumpInput) Jumping();
+        //if (fallInput) Falling();
+        if (WallCling) ClimbingWalls();
+    }
+
+    void RegisterInputs()
+    {
+        leftInput = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        rightInput = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        jumpInput = Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
+        fallInput = Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S);
+    }
+
+    void DetermineMovement()
+    {
+        movingLeft = leftInput && !rightInput;
+        movingRight = rightInput && !leftInput;
+    }
+
+    void HorizontalMovement()
+    {
+        if (movingLeft && !WallDetection(Vector3.left) && !WallCling)
+        {
+            if (CanJump) transform.position += new Vector3(-GroundSpeed * Time.deltaTime, 0, 0);
+            else transform.position += new Vector3((-GroundSpeed/2) * Time.deltaTime, 0, 0);
+        }
+        if (movingRight && !WallDetection(Vector3.right) && !WallCling)
+        {
+            if (CanJump) transform.position += new Vector3(GroundSpeed * Time.deltaTime, 0, 0);
+            else transform.position += new Vector3((GroundSpeed/2) * Time.deltaTime, 0, 0);
+        }
+    }
+
+    bool WallDetection(Vector3 direction)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, RaycastDistance, 255)) return true;
+        else return false;
+    }
+
+    void Jumping()
+    {
+        if (CanJump)
+        {
+            rb.useGravity = true;
+            WallCling = false;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(0, JumpForce, 0);
+            CanJump = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //if (!CanJump)
+        //{
+        //    CanJump = WallDetection(Vector3.down);
+        //}
+        if (!WallCling) rb.AddForce(0, -ArtificialGravity, 0);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!CanJump && collision.gameObject.CompareTag("Wall"))
+        {
+            CanJump = true;
+        }
+        if (WallDetection(Vector3.left) || WallDetection(Vector3.right))
+        {
+            WallCling = true;
+        }
+    }
+
+    void ClimbingWalls()
+    {
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+    }
+
+    void Falling()
+    {
+        rb.AddForce(0, -JumpForce * 2, 0);
+    }
+}
