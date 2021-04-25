c541a9f26fc0ef1611bc828362ac74e8cbd5cf61
BaderKQ <61980817+BaderKQ@users.noreply.github.com>
2021-04-25 19:12:04 +0300

    Enemy Behavior and Objects

---
+++ Assets/Enemies/BulletBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    float moveSpeed = 10f;
+    Rigidbody2D rb;
+
+    ThePlayer target;
+    Vector2 moveDirection; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>(); 
+        target = GameObject.FindObjectOfType<ThePlayer>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy (gameObject, 3f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag=="Player"){
+            Debug.Log("Hit");
+        }
+    }
+}
