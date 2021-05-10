5a4b074f11b6d89be81302ae17407da91f6bb407
kevinAK99 <yl6260@nyu.edu>
2021-05-10 22:12:00 +0800

    Changes made to the skeleton level design

---
+++ Assets/Pendulum.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+
+
+    [SerializeField] float Speed = 1.0f;
+    [SerializeField] AudioSource spikeBallSFX;
+
+    float dir = 1f;
+
+    public float leftZ, rightZ;
+
+    private Rigidbody mRigidbody = null;
+
+    protected void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+
+        transform.eulerAngles += dir * new Vector3(0, 0, Speed);
+        //Debug.Log(transform.eulerAngles.z + "/" + leftZ);
+        if (dir > 0 && transform.eulerAngles.z < 180 && transform.eulerAngles.z >= rightZ)
+        {
+            dir = -1;
+        }
+        else if (dir < 0 && transform.eulerAngles.z > 180 && (transform.eulerAngles.z - 360) <= leftZ)
+        {
+            dir = 1;
+        }
+
+        if (dir > 0 && transform.eulerAngles.z <= 0)
+        {
+
+            spikeBallSFX.Play();
+        }
+        if (dir < 0 && transform.eulerAngles.z <= 0)
+        {
+
+            spikeBallSFX.Play();
+        }
+
+
+    }
+
+
+
+}
