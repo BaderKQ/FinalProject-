faa9cf5a2467b1eb15eda4ad490e0213431162c6
Jeremy Huml <jh5293@nyu.edu>
2021-05-10 02:38:07 -0400

    Enemy Hurt Script
    -Made it so player can hurt enemies

---
+++ Assets/EnemyHurt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyHurt : MonoBehaviour
+{
+    public float MaxHP = 1;
+    public float CurrentHP = 1;
+
+    void Start()
+    {
+        CurrentHP = MaxHP;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Blah");
+        if (collision.CompareTag("PlayerStrike"))
+        {
+            print("THE Blah");
+            CurrentHP -= 1;
+            if (CurrentHP <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
